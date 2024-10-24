e863f330ed8b9f87a6d64b4e7a178f96fb6e1451
mintydevdaz <mintydevdaz@proton.me>
2024-10-24 23:23:13 +1100

    added async client cls

--- src/web.py
+++ src/web.py
@@ -3,6 +3,31 @@ import logging
 import httpx
 
 
+class Client:
+    def __init__(self, headers: dict[str, str] | None = None) -> None:
+        self.session = httpx.AsyncClient(headers=headers)
+
+    async def get(self, url: str):
+        try:
+            response = await self.session.get(url)
+            response.raise_for_status()
+
+        except httpx.RequestError as exc:
+            logging.error(f"Error occurred while requesting '{url}': {exc}.")
+            return None
+
+        except httpx.HTTPStatusError as exc:
+            msg = f"Error response {exc.response.status_code} while requesting {url}."
+            logging.error(msg)
+            return None
+
+        else:
+            return response
+
+    async def close(self) -> None:
+        await self.session.aclose()
+
+
 def basic_request(url: str, headers: dict[str, str]) -> httpx.Response | None:
     try:
         response = httpx.get(url, headers=headers)
@@ -16,6 +41,6 @@ def basic_request(url: str, headers: dict[str, str]) -> httpx.Response | None:
         msg = f"Error response {exc.response.status_code} while requesting {url}."
         logging.error(msg)
         return None
-    
+
     else:
         return response
