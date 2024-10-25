8bf8d6c29b529b1ad6857b7fdcf096b019b34106
mintydevdaz <mintydevdaz@proton.me>
2024-10-25 14:45:22 +1100

    added save functionality

--- src/tools.py
+++ src/tools.py
@@ -1,12 +1,16 @@
+import json
 import logging
-import tomllib
+from datetime import datetime, timezone
 from pathlib import Path
+from typing import Iterable
+
+import tomllib
 
 
 class File:
     @staticmethod
     def path(route: str) -> str:
-        # Point to specified folder within project
+        "Points to root folder of project (i.e. nrl_scraper)."
         f = Path().absolute().joinpath(route)
 
         if f.exists():
@@ -14,6 +18,20 @@ class File:
 
         msg = f"The specified path '{f}' does not exist!"
         raise FileNotFoundError(msg)
+
+
+def utc_date(format: str = "%Y-%m-%d %H:%M:%S%z") -> str:
+    now: datetime = datetime.now(timezone.utc)
+    return now.strftime(format)
+
+
+def save_to_json(data_object: Iterable, route: str, filename: str) -> None:
+    filepath: str = f"{route}/{filename}.json"
+    date: str = utc_date()
+    file_object: dict = {"date": date, "data": data_object}
+    with open(filepath, "w") as f:
+        json.dump(file_object, f)
+    print(f"File saved to JSON at '{filepath}'")
 
 
 def logger(filepath: str) -> None:
